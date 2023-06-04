2519cf918208c328b18544288bcd39fd99b4906b
Santiago Kozak <santikzk1406@gmail.com>
2023-06-04 20:43:34 -0300

    bump version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="leaguepedia-scoreboard-parser",
-    version="0.1.8",
+    version="0.1.9",
     author="RheingoldRiver",
     author_email="river.esports@gmail.com",
     description="Parser for Leaguepedia scoreboards",
