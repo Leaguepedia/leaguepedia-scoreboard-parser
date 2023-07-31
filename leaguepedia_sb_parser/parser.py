8c7120418def861f32f3547b5510bf12f5199399
Santiago Kozak <santikzk1406@gmail.com>
2023-07-31 16:03:10 -0300

    Change wording for missing short warning

--- leaguepedia_sb_parser/parser.py
+++ leaguepedia_sb_parser/parser.py
@@ -123,9 +123,9 @@ class Parser(object):
             # could be None either (a) because cannot find the team name
             # or (b) because it's live server and actually the original name is None and then gg us
             self.warnings.append(
-                'Final team name for {} is missing (original: `{}`) - maybe you have to set |short= in TeamRoster if the \
-                 code differs from what we use in Teamnames. You may also need to RefreshTeamnames or blank edit \
-                the team page. Inferred event: {}'.format(
+                'Final team name for {} is missing (original: `{}`) - maybe you have to set |short= in the \
+                 Participants section if the code differs from what we use in Teamnames. You may also need to \
+                 RefreshTeamnames or blank edit the team page. Inferred event: {}'.format(
                     team_key, team_name, self.event
                 )
             )
