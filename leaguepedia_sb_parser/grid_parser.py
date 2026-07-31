aacfdc4c8f5be0ac661079aa205db2eec7c808f4
arbolitoloco1 <santiago@arbolitoloco.xyz>
2026-07-31 11:14:33 -0300

    Revert "changes for qq game data from grid"
    This reverts commit 72f52ec26f008789680857e37ffd4ae57ad7de06.

--- leaguepedia_sb_parser/grid_parser.py
+++ leaguepedia_sb_parser/grid_parser.py
@@ -1,4 +1,4 @@
-from leaguepedia_sb_parser.components.fetch_game import get_riot_game_from_grid, get_riot_game_from_wiki
+from leaguepedia_sb_parser.components.fetch_game import get_game_from_grid, get_game_from_wiki
 from leaguepedia_sb_parser.parser import Parser
 
 
@@ -32,9 +32,9 @@ class GridParser(Parser):
 
     def parse_game(self, platform_game_id):
         if self.use_leaguepedia_mirror:
-            game = get_riot_game_from_wiki(platform_game_id, self.site)
+            game = get_game_from_wiki(platform_game_id, self.site)
         else:
-            game = get_riot_game_from_grid(platform_game_id)
+            game = get_game_from_grid(platform_game_id)
         self.populate_teams(game)
         output = self.parse_one_game(
             game, platform_game_id, key="riot_platform_game_id"
--- leaguepedia_sb_parser/qq_parser.py
+++ leaguepedia_sb_parser/qq_parser.py
@@ -1,29 +1,31 @@
 import re
 
+from lol_qq_parser.parsers.match_detail import get_series_basic_info
+
 from leaguepedia_sb_parser.parser import Parser
-from leaguepedia_sb_parser.components.fetch_game import get_tencent_series_from_grid
 
 from mwrogue.esports_client import EsportsClient
 
 
 class QQParser(Parser):
-    statslink = "grid_series_id"
-
-    initial_name_cache = {}
-
-    def parse_series(self, series_id, include_header=True):
-        series = get_tencent_series_from_grid(series_id)
-        self.patch = self.get_patch(series_id)
+    def parse_series(self, match_id, include_header=True):
+        match_id = int(match_id)
+        series = get_series_basic_info(match_id)
+        self.patch = self.get_patch(match_id)
         output_parts = []
         warnings = []
-        for _, game in enumerate(series.games):
+        for i, game in enumerate(series.games):
             self.populate_teams(game)
-            output_parts.append(self.parse_one_game(game, series_id, key="grid_series_id"))
+            output_parts.append(self.parse_one_game(game, self.qq_url(match_id)))
             warnings.extend(self.warnings)
             self.clear_warnings()
         if include_header:
             output_parts.insert(0, self.make_match_header())
         return "\n".join(output_parts), warnings
+
+    @staticmethod
+    def qq_url(match_id):
+        return "https://lpl.qq.com/es/stats.shtml?bmid={}".format(str(match_id))
 
     def parse_game(self, url):
         pass
@@ -40,26 +42,17 @@ class QQParser(Parser):
         return player.sources.qq.name
 
     def get_initial_team_name(self, team):
-        team_qq_id = team.sources.qq.id
+        if not hasattr(team.sources, "qq"):
+            return None
+        return team.sources.qq.tag.strip()
 
-        if team_qq_id in self.initial_name_cache:
-            return self.initial_name_cache[team_qq_id]
-
+    def get_patch(self, match_id):
         response = self.site.cargo_client.query(
-            tables="Teamnames=TN",
-            fields="TN.Short",
-            where=f"TN.QQId = {team_qq_id}",
-            limit=1,
-        )
-        self.initial_name_cache[team_qq_id] = response[0]["Short"]
-        return self.initial_name_cache[team_qq_id]
-
-    def get_patch(self, series_id):
-        response = self.site.cargo_client.query(
-            tables="MatchSchedule=MS",
+            tables="MatchScheduleGame=MSG, MatchSchedule=MS",
             fields="MS.LegacyPatch=Patch",
-            where=f"MS.GridSeriesId = '{series_id}'",
+            where=f"MSG.MatchHistory = 'https://lpl.qq.com/es/stats.shtml?bmid={match_id}'",
             limit=1,
+            join_on="MSG.MatchId=MS.MatchId",
         )
         if response and response[0]["Patch"] is not None:
             self.warnings.append("Patch was obtained from MatchSchedule!")
